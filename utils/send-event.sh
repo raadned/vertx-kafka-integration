829b659d9069da0587baaf01a022ae6c989d71c1
Radu <rnedelcu93@gmail.com>
2021-04-05 19:46:59 +0100

    refactor: rename utils script variables

--- utils/send-event.sh
+++ utils/send-event.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+EVENTS_ENDPOINT="http://localhost:8080/events"
 if [ $# -eq 0 ]; then
     echo "Payload file needs to be provided!"
     exit 1
 fi
 
-payload=$1
+EVENT_PAYLOAD=$1
 
-POST_EVENT="http://localhost:8080/events"
-
-curl -X POST -d @$payload $POST_EVENT --header "Content-Type:application/json"
+curl -X POST -d @$EVENT_PAYLOAD $EVENTS_ENDPOINT --header "Content-Type:application/json"
 
